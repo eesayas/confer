bc50ab2a07b01b981494fd9d69dceb271f7443db
eesayas <briones@ualberta.ca>
2020-12-12 22:17:37 -0700

    added screen share initial capability

--- public/javascripts/video.js
+++ public/javascripts/video.js
@@ -24,6 +24,26 @@ const peers = {};
 // define camera over multiple browers
 const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
+// trigger this function if you want to share your screen
+const shareScreen = () => {
+    // add screen share
+    navigator.mediaDevices.getDisplayMedia({audio: true, video: true}).then(stream => {
+        const video = document.getElementById("spotlight");
+        video.muted = true;
+        video.srcObject = stream;
+        video.addEventListener("loadedmetadata", () => {
+            video.play();
+        });
+
+        for (let [key, value] of myPeer._connections.entries()) {
+            console.log(key)
+            myPeer.call(key, stream, {metadata : {shareScreen: true}});
+        }
+    }).catch(err => {
+
+    });
+}
+
 navigator.mediaDevices.getUserMedia({
     video: true, audio: true
 }).then(stream => {
@@ -33,17 +53,27 @@ navigator.mediaDevices.getUserMedia({
     // on call to peers
     myPeer.on("call", call => {
 	    call.answer(stream);
-        const video = document.createElement("video");
+        let video = document.createElement("video");
         
         call.on("stream", (userVideoStream) => {
-            console.log(userVideoStream + "hey");
-		    addVideoStream(video, userVideoStream);
+
+            if(call.metadata && call.metadata.shareScreen == true){
+                console.log(call.metadata);
+                video = document.getElementById("spotlight");
+                video.muted = true;
+                video.srcObject = userVideoStream;
+                video.addEventListener("loadedmetadata", () => {
+                    video.play();
+                });
+
+            } else{
+                addVideoStream(video, userVideoStream);
+            }
         });
     });
 
     // on new user enter room send my stream to them
     socket.on("user-connected", user_id => {
-        console.log(user_id);
         connectToNewUser(user_id, stream);
     }); 
 });
@@ -67,7 +97,6 @@ connectToNewUser = (user_id, stream) => {
 
     // on receiving stream
     call.on("stream", (userVideoStream) => {
-	    console.log(userVideoStream);
         addVideoStream(video, userVideoStream);
     });
 
@@ -82,7 +111,7 @@ connectToNewUser = (user_id, stream) => {
 }
 
 // add video stream to video element
-addVideoStream = (video, stream) => {
+addVideoStream = (video, stream) => {;
     video.srcObject = stream;
     video.addEventListener("loadedmetadata", () => {
         video.play();
